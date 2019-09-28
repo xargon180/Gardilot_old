48af8c483eb6b3f181f876f4e890b29015e75116
Stefan Fröhlich <xargon180@gmail.com>
2019-09-28 19:16:58 +0200

    Upgrade to core 3.0.

--- Gardilot.Blazor/Client/Startup.cs
+++ Gardilot.Blazor/Client/Startup.cs
@@ -8,7 +8,10 @@ namespace Gardilot.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddFluxor(options => options.UseDependencyInjection(typeof(Startup).Assembly));
+            services.AddFluxor(options => options
+                .UseDependencyInjection(typeof(Startup).Assembly)
+                .AddMiddleware<Blazor.Fluxor.Routing.RoutingMiddleware>()
+            );
         }
 
         public void Configure(IComponentsApplicationBuilder app)
